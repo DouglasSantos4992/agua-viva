a451e7d3b926624ce09c12f552886aeb00d59f96
DouglasSantos4992 <douglassantos491092@gmail.com>
2026-04-17 11:19:11 -0300

    configura runtime vercel

--- api/upload.ts
+++ api/upload.ts
@@ -1,3 +1,15 @@
+import { put } from "@vercel/blob";
+
 export default async function handler(req: any, res: any) {
-  return res.status(200).json({ ok: true });
+  try {
+    const blob = await put("teste.txt", "teste funcionando", {
+      access: "public",
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    });
+
+    return res.status(200).json(blob);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Erro no upload" });
+  }
 }
