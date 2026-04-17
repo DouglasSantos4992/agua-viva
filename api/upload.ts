af0459e9cd099fa4a38dccdaff067261f87966c8
DouglasSantos4992 <douglassantos491092@gmail.com>
2026-04-17 11:35:09 -0300

    corrige req nao usado

--- api/upload.ts
+++ api/upload.ts
@@ -1,10 +1,3 @@
-export default function handler(req: any, res: any) {
-  try {
-    return res.status(200).json({ ok: true });
-  } catch (error: any) {
-    console.error("ERRO INTERNO:", error);
-    return res.status(500).json({
-      error: error?.message || "erro interno",
-    });
-  }
+export default function handler(_: any, res: any) {
+  res.status(200).json({ ok: true });
 }
