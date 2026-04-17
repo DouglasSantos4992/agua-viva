5bb0fb212c9f3dcb4fe0f4f5f7a248c0ffc836d1
DouglasSantos4992 <douglassantos491092@gmail.com>
2026-04-17 15:35:32 -0300

    lista arquivos blob

--- api/files.ts
+++ api/files.ts
@@ -1,21 +1,14 @@
 import { list } from "@vercel/blob";
 
-export default async function handler(req: any, res: any) {
-  try {
-    const { url } = req.query;
+export default async function handler(_: any, res: any) {
+  const { blobs } = await list({
+    token: process.env.BLOB_READ_WRITE_TOKEN,
+  });
 
-    const { blobs } = await list({
-      token: process.env.BLOB_READ_WRITE_TOKEN,
-    });
-
-    const file = blobs.find((b) => b.url === url);
-
-    if (!file) {
-      return res.status(404).json({ error: "Arquivo não encontrado" });
-    }
-
-    return res.redirect(file.downloadUrl);
-  } catch (err: any) {
-    return res.status(500).json({ error: err.message });
-  }
+  return res.status(200).json(
+    blobs.map((file) => ({
+      nome: file.pathname,
+      downloadUrl: file.downloadUrl,
+    }))
+  );
 }
