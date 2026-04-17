2cccbddcbd65bd177f6fa486a32f6aaf1d30f3ee
DouglasSantos4992 <douglassantos491092@gmail.com>
2026-04-17 11:26:23 -0300

    corrige runtime api

--- api/upload.ts
+++ api/upload.ts
@@ -1,3 +1,10 @@
 export default function handler(req: any, res: any) {
-  res.status(200).json({ ok: true });
+  try {
+    return res.status(200).json({ ok: true });
+  } catch (error: any) {
+    console.error("ERRO INTERNO:", error);
+    return res.status(500).json({
+      error: error?.message || "erro interno",
+    });
+  }
 }
