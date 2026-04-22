aee99a1a69f2345537ddebf9566314e7fa8fd753
DouglasSantos4992 <douglassantos491092@gmail.com>
2026-04-22 11:52:48 -0300

    fix name

--- api/upload.ts
+++ api/upload.ts
@@ -29,6 +29,8 @@ export default async function handler(req: any, res: any) {
 
       const fileBuffer = fs.readFileSync(file.filepath);
 
+      console.log("Nome original:", file.originalFilename);
+
       const safeName =
         file.originalFilename
           ?.normalize("NFD")
